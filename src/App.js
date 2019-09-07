75b59406913161c1c88cd3a6b2630002aebeb7e2
Richard Hulse <richard@hulse.nz>
2019-09-07 14:07:11 +1200

    Screensaver closes any open gloss
    Move some app code into Global scope to allow
    gloss.hide to be used.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
-import React, { useRef, useCallback } from "react";
-import IdleTimer from "react-idle-timer";
+import React from "react";
 import GlobalState from "./contexts/GlobalState";
-
-import ScreenSaver from "./components/ScreenSaver";
-import config from "./configuration";
 import Kiosk from "./components/Kiosk";
 
 function App() {
-  const idleTimer = useRef(null);
-  const screenSaver = useRef(null);
-
-  const appIsActive = useCallback(() => {
-    screenSaver.current.stop();
-  }, []);
-
-  const appIsIdle = useCallback(() => {
-    screenSaver.current.start();
-  }, []);
-
   return (
     <>
-      <ScreenSaver ref={screenSaver} />
-      <IdleTimer
-        ref={idleTimer}
-        onActive={appIsActive}
-        onIdle={appIsIdle}
-        debounce={250}
-        timeout={config.screensaver.seconds_timeout_to_start}
-        startOnLoad
-      />
       <GlobalState>
         <Kiosk />
       </GlobalState>
--- src/components/Kiosk.js
+++ src/components/Kiosk.js
@@ -1,6 +1,10 @@
-import React, { useRef, useContext, useEffect } from "react";
+import React, { useRef, useContext, useEffect, useCallback } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { PrefsContext, ADD_GLOSS } from "../contexts/GlobalState";
+
+import IdleTimer from "react-idle-timer";
+import ScreenSaver from "./ScreenSaver";
+import config from "../configuration";
 
 import Header from "./Header";
 import Main from "./Main";
@@ -8,7 +12,19 @@ import Gloss from "./Gloss";
 
 export default function Kiosk() {
   const glossRef = useRef(null);
+  const idleTimer = useRef(null);
+  const screenSaver = useRef(null);
+
   const { dispatch } = useContext(PrefsContext);
+
+  const appIsActive = useCallback(() => {
+    screenSaver.current.stop();
+  }, []);
+
+  const appIsIdle = useCallback(() => {
+    glossRef.current.hide();
+    screenSaver.current.start();
+  }, []);
 
   useEffect(() => {
     dispatch({ type: ADD_GLOSS, gloss: glossRef.current });
@@ -16,6 +32,15 @@ export default function Kiosk() {
 
   return (
     <>
+      <ScreenSaver ref={screenSaver} />
+      <IdleTimer
+        ref={idleTimer}
+        onActive={appIsActive}
+        onIdle={appIsIdle}
+        debounce={250}
+        timeout={config.screensaver.seconds_timeout_to_start}
+        startOnLoad
+      />
       <div className="container">
         <BrowserRouter>
           <Header />
