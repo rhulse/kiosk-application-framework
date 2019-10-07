a8c2c0f02c425e1ac6d9658bb03f753de6069d0e
Richard Hulse <richard@hulse.nz>
2019-10-07 18:47:53 +1300

    Add some comments

--- src/utils/dom-events.js
+++ src/utils/dom-events.js
@@ -6,20 +6,23 @@ export const STOP_MEDIA_EVENT = "stopMediaPlaying";
 const mediaIsPlayingEvent = new Event(MEDIA_IS_PLAYING);
 const stopMediaPlayingEvent = new Event(STOP_MEDIA_EVENT);
 
-// playing events are emitted by media players in order
-// to stop the screensaver starting
+// media players should emit this event at regular intervals to stop the screensaver starting
 export const dispatchPlayingEvent = () => {
   document.dispatchEvent(mediaIsPlayingEvent);
 };
 
+// media players should listen for this event using the hook or functions below
+// this event is only sent on a user action - media will time out if no other user action follows
 export const dispatchStopMediaEvent = () => {
   document.dispatchEvent(stopMediaPlayingEvent);
 };
 
+// use in functional components
 export function useStopMediaEventListener(callback) {
   useEventListener(STOP_MEDIA_EVENT, callback, document);
 }
 
+// use in class components
 export function addStopMediaEventListener(callback) {
   document.addEventListener(STOP_MEDIA_EVENT, callback);
 }
