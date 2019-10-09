e4ffc23f9f2920c055b867e149d8cb33d305f9d8
Richard Hulse <richard@hulse.nz>
2019-10-09 17:59:35 +1300

    Changes missed from earlier commit

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,7 +1,10 @@
 import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
 export {
   faVolumeUp as speaker,
-  faTimes as close
+  faTimes as close,
+  faChevronLeft as chevronLeft,
+  faChevronRight as chevronRight,
+  faHome as home
 } from "@fortawesome/free-solid-svg-icons";
 
 export default Icon;
