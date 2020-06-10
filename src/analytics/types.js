5448145aa60b5c2ef45488ad04fa32db79c47654
Richard Hulse <richard@hulse.nz>
2020-06-10 21:06:51 +1200

    Remove analytics and use npm package
    This package has the same code, extracted and refined.

--- src/analytics/types.js
+++
@@ -1,8 +0,0 @@
-// types uses by the anaytics providers
-
-export const SET_LANGUAGE = "SET_LANGUAGE";
-export const PAGE_VIEW = "PAGE_VIEW";
-export const EVENT = "EVENT";
-export const TIMING = "TIMING";
-export const SESSION = "SESSION";
-export const SET_PAGE = "SET_PAGE";
--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,20 +1,18 @@
-import AnalyticsDispatcher from "../analytics/AnalyticsDispatcher";
-import GoogleAnalyticsProvider from "../analytics/providers/googleGA";
+import { AnalyticsDispatcher, GoogleGA } from "analytics-provider";
 
 import config from "../configuration";
 
-const googleProvider = new GoogleAnalyticsProvider({
+const googleProvider = new GoogleGA({
   providerId: config.analytics.providers.googleGA,
-  defaultLanguage: config.i18n.defaultLocale,
-  applicationName: config.application.name,
-  applicationVersion: config.application.version,
   loggingLevel: config.analytics.loggingLevel,
   debug: config.analytics.debug,
 });
 
 export const analytics = new AnalyticsDispatcher({
   providers: [googleProvider],
-  idleTimeout: config.screenSaver.idleTimeout * 1000,
-  useEstimatedSessionTiming: config.analytics.useEstimatedSessionTiming,
   loggingLevel: config.analytics.loggingLevel,
+  trackPageTime: true,
+  trackSessionTime: true,
+  idleTimeout: config.screenSaver.idleTimeout * 1000,
+  adjustSessionTimingForTimeout: config.analytics.useEstimatedSessionTiming,
 });
